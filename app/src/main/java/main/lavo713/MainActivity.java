0ff48c03eea9ba2820d03fdc91c827206b25d129
AtheistJesus <atheistjesus@users.noreply.github.com>
2015-07-19 19:43:22 -0700

    Added settings page with link to website

--- app/src/main/java/main/lavo713/MainActivity.java
+++ app/src/main/java/main/lavo713/MainActivity.java
@@ -244,6 +244,8 @@ public class MainActivity extends ListActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent(this,SettingsActivity.class);
+            startActivity(i);
             return true;
         }
 
